62cea54fba5b7143cf56f3902017474c9741a18a
Grzegorz Myjak <g.myjak@neurosys.com>
2021-10-29 12:54:58 +0200

    xD

--- zad2/main.py
+++ zad2/main.py
@@ -1,15 +1,25 @@
 import numpy
 from mnist import MNIST
 
-# todo bias -> wagi input + 1, dodać 1 na końcu wektora wejściowego
+
+# jak zrobić to zadanie
+# uaktualnianie wag - dla każdej warstwy:
+#   zapamiętać aktywację - to łatwe
+#   policzyć tchibo
+# żeby policzyć tchibo:
+#   gradient softmax - hardcode?
+#   inne gradienty :)
 
 class Layer:
-    def __init__(self, input_size, output_size, function):
+    def __init__(self, input_size, output_size, function, function_der, smax = False):
         self.input_size = input_size
         self.output_size = output_size
         self.function = function
+        self.function_der = function_der
+        self.smax = smax
         self.bias = []
         self.weights = []
+        self.last_activation = []
 
 
     def init_weights(self, std_dev):
@@ -23,7 +33,19 @@ class Layer:
 
 
     def activated_output(self, input):
-        return [self.function(z) for z in self.output(input)]
+        if self.smax:
+            self.last_activation = self.function(self.output(input))
+        else:
+            self.last_activation = [self.function(z) for z in self.output(input)]
+        return self.last_activation
+
+
+    def nl_error(self, sample, correct):
+        sum = 0
+        for j in len(sample):
+            sum += -numpy.log(self.last_activation[j]) * correct[j]
+        return sum
+
 
 
 class MLP:
@@ -44,25 +66,53 @@ class MLP:
         output = input
         for layer in self.layers:
             output = layer.activated_output(output)
-        return softmax(output)
+        return output
+
+
+    def learn(self, sample):
+        self.predict(sample)
+
 
 
 def relu(z):
     return z if z >= 0 else 0
 
 
+def relu_der(z):
+    if z == 0:
+        return 0.5
+    else: return 0 if z < 0 else 1
+
+
 def sigmoid(z):
     return 1 / (1 + numpy.e ** -z)
+
+
+def sigmoid_der(z):
+    sigm = sigmoid(z)
+    return sigm * (1 - sigm)
 
 
 def tanh(z):
     return 2 / (1 + numpy.e ** (-2 * z)) - 1
 
 
+def tanh_der(z):
+    th = tanh(z)
+    return 1 - (th * th)
+
+
 def softmax(results):
     results_e = [numpy.e ** a for a in results]
+    for a in results:
+        print(a)
     sum = numpy.sum(results_e)
     return [ezj / sum for ezj in results_e]
+
+
+def softmax_der(results):
+    smax = softmax(results)
+    return [-()]
 
 
 def main():
@@ -75,23 +125,23 @@ def test_data():
     images, labels = mndata.load_training()
 
     model = MLP()
-    model.add_layer(Layer(784, 300, tanh).init_weights(0.1))
-    model.add_layer(Layer(300, 300, tanh).init_weights(1))
-    model.add_layer(Layer(300, 10, tanh).init_weights(1))
+    model.add_layer(Layer(784, 300, relu, relu_der).init_weights(0.01))
+    model.add_layer(Layer(300, 300, relu, relu_der).init_weights(0.01))
+    model.add_layer(Layer(300, 10, softmax, None, True).init_weights(0.01))
     print("PREDICTION:", model.predict(images[0]))
     print("REAL:", labels[0])
 
 
 def test_mlp():
     model = MLP()
-    model.add_layer(Layer(3, 5, relu).init_weights(1))
-    model.add_layer(Layer(5, 5, relu).init_weights(1))
-    model.add_layer(Layer(5, 4, relu).init_weights(1))
+    model.add_layer(Layer(3, 10, relu, relu_der).init_weights(1))
+    model.add_layer(Layer(10, 15, relu, relu_der).init_weights(1))
+    model.add_layer(Layer(15, 5, softmax, None, True).init_weights(1))
     print(model.predict([0.2, 0.6, 0.3]))
 
 
 def test_layer():
-    model = Layer(2, 3, relu)
+    model = Layer(2, 3, relu, relu_der)
     model.init_weights(1.4)
     print(model.weights)
     print(model.bias)
